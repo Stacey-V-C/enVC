b2e629ad4bb68de9af5f477e40af61b7bfb2fb73
Stacey Carroll <stacey.v.carroll@gmail.com>
2023-08-22 18:27:38 -0500

    Chore: match styles and type in ui_receiver

--- services/ui_receiver.go
+++ services/ui_receiver.go
@@ -7,35 +7,35 @@ import (
 )
 
 type UIReceiver struct {
-	ctx          context.Context
-	sql_channel  chan types.NVC_Event
-	held_results map[string][]string
+	ctx         context.Context
+	sqlChannel  chan types.NVC_Event
+	heldResults map[string][]string
 }
 
-func NewUIReceiver(ctx context.Context, sql_channel chan types.NVC_Event) *UIReceiver {
+func NewUIReceiver(ctx context.Context, sqlChannel chan types.NVC_Event) *UIReceiver {
 	return &UIReceiver{
-		ctx:          ctx,
-		sql_channel:  sql_channel,
-		held_results: make(map[string][]string),
+		ctx:         ctx,
+		sqlChannel:  sqlChannel,
+		heldResults: make(map[string][]string),
 	}
 }
 
 func (u *UIReceiver) Listen() {
-	go ListenAndStoreResults(u, u.sql_channel)
+	go ListenAndStoreResults(u, u.sqlChannel)
 }
 
 func ListenAndStoreResults(u *UIReceiver, channel chan types.NVC_Event) {
 	for input := range channel {
 		fmt.Println("UIReceiver received:", input)
-		results := [2]string{input.Payload, input.Action}
-		u.held_results[input.Id] = results[:]
+		results := [2]string{*input.Payload, input.Action}
+		u.heldResults[input.Id] = results[:]
 	}
 }
 
 func (u *UIReceiver) PullData(id string) []string {
-	return u.held_results[id]
+	return u.heldResults[id]
 }
 
 func (u *UIReceiver) GetReceiveChannel() chan types.NVC_Event {
-	return u.sql_channel
+	return u.sqlChannel
 }
