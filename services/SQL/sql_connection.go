263a988054c0b5022b7d85f582d0639be0a57125
Stacey Carroll <stacey.v.carroll@gmail.com>
2023-08-22 21:41:23 -0500

    Chore: add db initiation routine

--- services/SQL/sql_connection.go
+++ services/SQL/sql_connection.go
@@ -26,6 +26,12 @@ func NewSQLConnection(ctx context.Context) SQLConnection {
 		panic(err)
 	}
 
+	err = ConnectToDefault(db)
+
+	if err != nil {
+		panic(err)
+	}
+
 	return SQLConnection{
 		ctx,
 		db,
@@ -60,4 +66,124 @@ func (s *SQLConnection) GetResultSetAsStrings(q string, len int) ([][]string, er
 	}
 
 	return resultSet, nil
+}
+
+func ConnectToDefault(db *sql.DB) error {
+	dbName, exists := os.LookupEnv("SQL_DB_NAME")
+
+	if exists == false {
+		dbName = "envc"
+	}
+
+	_, err := db.Exec(
+		"CREATE DATABASE IF NOT EXISTS " + dbName + ";" +
+			"USE " + dbName + ";",
+	)
+
+	return err // even if nil
+}
+
+func (s *SQLConnection) Init() error {
+	queries := []string{
+		`CREATE TABLE IF NOT EXISTS topics (
+			id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			title VARCHAR(64) NOT NULL,
+			description TEXT,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`,
+
+		`CREATE TABLE IF NOT EXISTS comments (
+			id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			p_type ENUM(
+				'topic', 
+				'tag', 
+				'tag_type', 
+				'comment', 
+				'connection', 
+				'directed_relation
+			) NOT NULL DEFAULT 'topic',
+			p_id INT NOT NULL,
+			content TEXT,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`,
+
+		`CREATE TABLE IF NOT EXISTS tags (
+			id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			p_type ENUM(
+				'topic',
+				'tag',
+				'tag_type',
+				'comment',
+				'connection',
+				'directed_relation'
+			) NOT NULL DEFAULT 'topic',
+			p_id INT NOT NULL,
+			type INT NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`,
+
+		`CREATE TABLE IF NOT EXISTS tag_types (
+			id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			name VARCHAR(128) NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`,
+
+		`CREATE TABLE IF NOT EXISTS connections (
+			id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			p_1_type ENUM(
+				'topic',
+				'tag',
+				'tag_type',
+				'comment',
+				'connection',
+				'directed_relation'
+			) NOT NULL DEFAULT 'topic',
+			p_1_id INT NOT NULL,
+			p_2_type ENUM(
+				'topic',
+				'tag',
+				'tag_type',
+				'comment',
+				'connection',
+				'directed_relation'
+			) NOT NULL DEFAULT 'topic',
+			p_2_id INT NOT NULL,
+			content TEXT,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`,
+
+		`CREATE TABLE IF NOT EXISTS directed_relations (
+			id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			s_p_type ENUM(
+				'topic',
+				'tag',
+				'tag_type',
+				'comment',
+				'connection',
+				'directed_relation'
+			) NOT NULL DEFAULT 'topic',
+			s_p_id INT NOT NULL,
+			e_p_type ENUM(
+				'topic',
+				'tag',
+				'tag_type',
+				'comment',
+				'connection',
+				'directed_relation'
+			) NOT NULL DEFAULT 'topic',
+			e_p_id INT NOT NULL,
+			content TEXT,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`,
+	}
+
+	err := ConnectToDefault(s.db)
+
+	if err != nil {
+		return err
+	}
+
+	for _, q := range queries {
+		_, err := s.db.ExecContext(s.ctx, q)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
