3a9ba318eab4ded31c626f5e9ab9ddf5c5ba64fe
Stacey Carroll <stacey.v.carroll@gmail.com>
2023-09-01 07:10:22 -0500

    Feat: more typing: migrations, improved generics

--- types/types.go
+++ types/types.go
@@ -13,6 +13,13 @@ const (
 	UI       EventType = iota
 )
 
+type DataSource int8
+
+const (
+	SOURCE_SQL    DataSource = iota
+	SOURCE_CHROMA DataSource = iota
+)
+
 type ErrorType int8
 
 const (
@@ -25,11 +32,11 @@ const (
 type ActionType string
 
 const (
-	ERROR                 ActionType = "error"
-	SQL_RAW_RESULT        ActionType = "sql_raw_result"
-	SQL_FILTER_RESULT     ActionType = "sql_filter_result"
-	MIGRATE_SQL_TO_CHROMA ActionType = "migrate_sql_to_chroma"
-	SQL_QUERY             ActionType = "sql_query"
+	ERROR             ActionType = "error"
+	SQL_RAW_RESULT    ActionType = "sql_raw_result"
+	SQL_FILTER_RESULT ActionType = "sql_filter_result"
+	MIGRATE           ActionType = "migrate_sql_to_chroma"
+	SQL_QUERY         ActionType = "sql_query"
 )
 
 type ReceiverMap map[EventType]chan NVC_Event // one event channel per event type
@@ -42,10 +49,14 @@ type NVC_Event struct {
 	Payload any        `json:"payload"`
 }
 
-type Typed_NVC_Event[T any] struct {
+type Type_NVC_Event[T NVC_Payload] struct {
 	Action  ActionType `json:"action"`
 	Id      string     `json:"id"`
-	Payload T          `json:"payload"`
+	Payload *T         `json:"payload"`
+}
+
+type NVC_Payload interface {
+	string | TypedError | SQLResultPayload
 }
 
 type TypedError struct {
@@ -60,17 +71,37 @@ type SQLResultPayload struct {
 	Values    [][]string
 }
 
+type MigrationPayload interface {
+	SQLResultPayload
+}
+
+type Migration struct {
+	from DataSource
+	to   DataSource
+	Data any
+}
+
+func (m *Migration) Validate() bool {
+	switch m.from {
+	case SOURCE_SQL:
+		valid := reflect.TypeOf(m.Data) == reflect.TypeOf(&[]SQLResultPayload{})
+
+		return valid
+	}
+	return false
+}
+
 var emptyString = ""
 
 var requiredTypes = map[ActionType]reflect.Type{
-	ERROR:                 reflect.TypeOf(&TypedError{}),
-	SQL_RAW_RESULT:        reflect.TypeOf(&SQLResultPayload{}),
-	SQL_FILTER_RESULT:     reflect.TypeOf(&SQLResultPayload{}),
-	MIGRATE_SQL_TO_CHROMA: reflect.TypeOf(&[]NVC_Event{}),
-	SQL_QUERY:             reflect.TypeOf(&emptyString),
+	ERROR:             reflect.TypeOf(&TypedError{}),
+	SQL_RAW_RESULT:    reflect.TypeOf(&SQLResultPayload{}),
+	SQL_FILTER_RESULT: reflect.TypeOf(&SQLResultPayload{}),
+	MIGRATE:           reflect.TypeOf(&Migration{}),
+	SQL_QUERY:         reflect.TypeOf(&emptyString),
 }
 
-func (n *NVC_Event) validate() bool {
+func (n *NVC_Event) Validate() bool {
 	return reflect.TypeOf(n.Payload) == requiredTypes[n.Action]
 }
 
